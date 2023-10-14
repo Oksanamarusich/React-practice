1e7d7e4876fa7adc0712fb681bd02322ad6f0be9
Oksanamarusich <oksana.m229@gmail.com>
2023-10-14 13:32:10 +0300

    add modal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,14 +3,13 @@ import {ImageGalleryItem} from "../ImageGalleryItem/ImageGalleryItem"
 export const ImageGallery = ({ gallery }) => {
     return <div>
         {gallery.map(({ id, webformatURL, largeImageURL, tags }) => (
-            <ul key = {id}>
-                <ImageGalleryItem key={id}
+            <ul key={id}>
+                <ImageGalleryItem 
+                    id={id}
                     picture={webformatURL}
                     picturemodal={largeImageURL}
-                alt = {tags}>
-                    <img src={webformatURL} alt = {tags} />
-                </ImageGalleryItem>
-        </ul>
+                    alt = {tags}/>
+            </ul>
         ))}
     
     </div>
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,19 @@
 import Modal from 'react-modal';
 import { Component } from 'react';
+//import { Overlay, ContainerImg } from "../Modal/Modal.styled"
+import "../Modal/Modal.css"
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//   },
+// };
 
 Modal.setAppElement('#root');
 
@@ -29,24 +32,28 @@ export class ImageGalleryItem extends Component {
 
       render() {
             const { isModalOpen } = this.state;
-            const {
-                  card: { id, picture, picturemodal, alt },
-            } = this.props;
+            const { id, picture, picturemodal, alt } = this.props;
             return (<div>
                   <li key = {id}>
-                        <img stc={picture} alt={alt} onClick={this.openModal} />
+                        <img src={picture} alt={alt} onClick={this.openModal} />
                   </li>
 
                   <Modal
                         
                         isOpen={isModalOpen}
                         onRequestClose={this.closeModal}
-                        style={customStyles}
+                        // style={customStyles}
                         contentLabel="Example Modal"
+                         className="Modal"
+                         overlayClassName="Overlay"
+             
                   >
                         <div>
-                              <img src={picturemodal} alt={alt} l />
+                              <div>
+                                   <img src={picturemodal} alt={alt} />
+                              </div>
                         </div>
+                        
                   </Modal>
             </div>)
 
@@ -55,3 +62,8 @@ export class ImageGalleryItem extends Component {
 
 
 
+// export const ImageGalleryItem = ({id, picture,picturemodal, alt}) => {
+//       return <li key={id}>
+//             <img src={picture} alt={alt} />
+//       </li>
+// }
